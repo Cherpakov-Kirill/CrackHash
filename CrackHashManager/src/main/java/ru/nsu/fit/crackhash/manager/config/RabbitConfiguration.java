cd6ee0f8012f8b807501a6f02b8a7503786ea4b5
Cherpakov-Kirill <cherpakov-kirill-42@mail.ru>
2023-03-28 00:59:05 +0700

    Extracted Rabbit config to properties

--- CrackHashManager/src/main/java/ru/nsu/fit/crackhash/manager/config/RabbitConfiguration.java
+++ CrackHashManager/src/main/java/ru/nsu/fit/crackhash/manager/config/RabbitConfiguration.java
@@ -2,9 +2,9 @@ package ru.nsu.fit.crackhash.manager.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2XmlMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,18 +18,12 @@ public class RabbitConfiguration {
     @Value("${crackHashManager.rabbitmq.queue.manager-to-workers}")
     private String senderQueue;
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setPort(5672);
-        return connectionFactory;
-    }
+    @Autowired
+    private CachingConnectionFactory connectionFactory;
 
     @Bean
     public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(xmlMessageConverter());
         return rabbitTemplate;
     }
